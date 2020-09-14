0ef5b31cd7acbdab807fdbb382379249339f60e5
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2020-09-14 10:55:36 +0700

    learn done task6, template string

---
+++ task6_2.js
@@ -0,0 +1,10 @@
+// function hello(name){
+//     console.log('hello ' + name + ' !');
+//     return 'hello ' + name + ' !';
+// }
+function hello2(name){
+    console.log(`hello ${name} !`);
+    console.log(`hello ${name} ${1+23} !`);
+}
+// console.log(hello('Hoang'));
+hello2('Hoang');
