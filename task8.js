ada7666eb81448dc8f99b431358320072b506136
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2020-09-15 21:57:25 +0700

    learn done task8, default parameters && object, array using const util change

---
+++ task8.js
@@ -0,0 +1,37 @@
+// let name;
+// console.log(name = 'Hoang');
+
+// if (1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// const obj1 = {
+//     name: 'Hoang'
+// }
+// const obj2 = obj1;
+
+// console.log(obj1);
+// console.log(obj2);
+
+// obj2.name = 'Van';
+
+// console.log(obj1);
+// console.log(obj2);
+
+
+// const obj1 = {
+//     name: 'Hoang'
+// }
+// console.log(obj1);
+// obj1.name = 'Van';
+// console.log(obj1);
+
+const name = [1,2,3];
+console.log(name);
+console.log(name[1]);
+name[1] = 10;
+name[0] = 9;
+console.log(name);
+console.log(name[1]);
